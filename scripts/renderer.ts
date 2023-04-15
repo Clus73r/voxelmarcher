8d196e21deff2af9b43fe1afbcdb323c36d186e4
cluster <stephan@dasbach.de>
2023-04-15 10:44:42 +0200

    hi

--- scripts/renderer.ts
+++ scripts/renderer.ts
@@ -1,51 +1,51 @@
 class Renderer {
-  canvas: HTMLCanvasElement;
-  adapter: GPUAdapter | undefined;
-  device: GPUDevice | undefined;
-  context: GPUCanvasContext | undefined;
-  format: GPUTextureFormat | undefined;
+	canvas: HTMLCanvasElement;
+	adapter: GPUAdapter | undefined;
+	device: GPUDevice | undefined;
+	context: GPUCanvasContext | undefined;
+	format: GPUTextureFormat | undefined;
 
-  constructor(canvas: HTMLCanvasElement) {
-    this.canvas = canvas;
-  }
+	constructor(canvas: HTMLCanvasElement) {
+		this.canvas = canvas;
+	}
 
-  async Initialize() {
-    await this.setupDevice();
-    await this.setupPipeline();
-    this.render();
-  }
+	async Initialize() {
+		await this.setupDevice();
+		await this.setupPipeline();
+		this.render();
+	}
 
-  async setupDevice() {
-    this.adapter = <GPUAdapter>await navigator.gpu?.requestAdapter();
-    this.device = await this.adapter?.requestDevice();
-    this.context = <GPUCanvasContext>this.canvas.getContext("webgpu");
-    this.format = "bgra8unorm";
-    this.context.configure({
-      device: this.device,
-      format: this.format,
-      alphaMode: "opaque",
-    });
-  }
+	async setupDevice() {
+		this.adapter = <GPUAdapter>await navigator.gpu?.requestAdapter();
+		this.device = await this.adapter?.requestDevice();
+		this.context = <GPUCanvasContext>this.canvas.getContext("webgpu");
+		this.format = "bgra8unorm";
+		this.context.configure({
+			device: this.device,
+			format: this.format,
+			alphaMode: "opaque",
+		});
+	}
 
-  async setupPipeline() {}
+	async setupPipeline() { }
 
-  render = () => {
-    const commandEncoder = this.device?.createCommandEncoder();
-    const textureView = this.context?.getCurrentTexture().createView();
-    const renderPass = commandEncoder?.beginRenderPass({
-      colorAttachments: [
-        {
-          view: <GPUTextureView>textureView,
-          clearValue: { r: 0.5, g: 0.0, b: 0.25, a: 1.0 },
-          loadOp: "clear",
-          storeOp: "store",
-        },
-      ],
-    });
-    renderPass?.end();
+	render = () => {
+		const commandEncoder = this.device?.createCommandEncoder();
+		const textureView = this.context?.getCurrentTexture().createView();
+		const renderPass = commandEncoder?.beginRenderPass({
+			colorAttachments: [
+				{
+					view: <GPUTextureView>textureView,
+					clearValue: { r: 0.5, g: 0.0, b: 0.25, a: 1.0 },
+					loadOp: "clear",
+					storeOp: "store",
+				},
+			],
+		});
+		renderPass?.end();
 
-    this.device?.queue.submit([<GPUCommandBuffer>commandEncoder?.finish()]);
+		this.device?.queue.submit([<GPUCommandBuffer>commandEncoder?.finish()]);
 
-    requestAnimationFrame(this.render);
-  };
+		requestAnimationFrame(this.render);
+	};
 }
